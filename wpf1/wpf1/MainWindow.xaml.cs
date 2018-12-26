46e1c7f9324f1ee4f7fbc3433e8265a0c3d50fff
carrie222 <19910517882@163.com>
2018-12-26 10:16:00 +0800

    WPF 实现数据绑定

---
+++ wpf1/wpf1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            _Model = new MyModel();
+            this.DataContext = _Model;
+        }
+        private MyModel _Model;
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void OnShowData_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show(_Model.Data);
+        }
+
+        private void OnShowData_CanExecuted(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void OnClearData_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            _Model.Data = string.Empty;
+        }
+
+        private void OnClearData_CanExecuted(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _Model != null && !string.IsNullOrEmpty(_Model.Data);
+        }
+    }
+}
---
+++ wpf1/wpf1/MyModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace wpf1
+{
+    class MyModel:INotifyPropertyChanged
+    {
+        private string _Data = "My Data";
+        public string Data
+        {
+            get { return _Data; }
+            set {
+                if (_Data == value) return;
+                _Data = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("Data"));
+                 }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
